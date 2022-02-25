fece5dfe2e852a4eee80e2340e43a4c37982e04f
Jakob Hohenstein <jakob-hohenstein@gmx.de>
2022-02-25 15:09:36 +0100

    deleted unnecessary text

--- app.py
+++ app.py
@@ -7,32 +7,6 @@ import requests
 # TaxiFareModel front
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
 url = 'https://taxifare.lewagon.ai/predict'
 
 #if url == 'https://taxifare.lewagon.ai/predict':
@@ -64,17 +38,6 @@ df = pd.DataFrame({'lon': [float(pickup_longitude), float(dropoff_longitude)],
                    'lat': [float(pickup_latitude), float(dropoff_latitude)]})
 
 st.map(df)
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
 
 # 2. Build dictionary with parameters for API
 
